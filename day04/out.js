387fee6d9a06ae4ce22522c0a4f1aa0695c46a6c
baloo365 <124110711+baloo365@users.noreply.github.com>
2023-02-02 18:02:08 +0900

    day4 자바스크립트
    헷갈린 부분
    1. document.getElementById("아이디명") 개념
    - 아이디 확인 버튼-자바스크립트 연결
    input에 id, value 부여 / button on click="함수명()" / document.getElementById("아이디명").value 사용하여 변수 할당 (해당 아이디명 태그의 값을 할당함)
    - alert가 아닌 document 부분에 알림 뜨게 하기
    div에 아이디 부여 / document.getElementById("아이디명")  (해당 아이디명 div를 할당함)  /  변수명.innerHTML = "<font color =red>패스워드 동일 확인</font> <img src='이미지 주소' width=100 height=100>"
    document.getElementById("아이디명") -> 해당 아이디명의 태그를 가져온다는 얘기.
    2. 변수명
    - for 문이나 if 문 안에서 선언되고 할당된 변수는 그 조건문/반복문 안에서만 사용됨(?)

---
+++ day04/out.js
@@ -0,0 +1,6 @@
+
+          function 청소하다(){
+            console.log("1. 청소기를 가져온다")
+            console.log("2. 청소기를 돌린다")
+            console.log("3. 물건을 치운다")
+          }
---
+++ day04/out2.js
@@ -0,0 +1,7 @@
+function 여행하다() {
+    console.log("1. 다른 지역에 가서 힐링하는 시간을 보낸다.")
+}
+
+function 쉬다() {
+    console.log("1. 마음과 몸을 편히 하다.")
+}
